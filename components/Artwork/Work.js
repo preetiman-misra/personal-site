6cfe64cef06d162262ead0c99e95fe5b07c511c7
Preetiman Misra <pmisra50@gmail.com>
2021-08-29 16:30:35 +0530

    Changed image quality settings for better loading

--- components/Artwork/Work.js
+++ components/Artwork/Work.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Work({ src, title }) {
   return (
     <div className="work">
-      <Image src={src} alt={title} quality={100} />
+      <Image src={src} alt={title} />
     </div>
   );
 }
